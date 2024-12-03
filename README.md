b745044d3d4ac55cea8f065dd677a769373553c4
changsyuanwu <william.wu1219@gmail.com>
2024-12-03 15:57:50 +0800

    e3.9, 3.10 combined with frontend and deployed to fly.io

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # fullstackopen-phonebook-backend
+
+Node.js + Express backend for the phonebook exercise.
+
+Created as part of the Full Stack Open course.
+
+View online at https://fullstackopen-phonebook-backend-chang.fly.dev/
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(express.static("dist"));
+app.use(cors())
 
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
@@ -13,7 +16,7 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
