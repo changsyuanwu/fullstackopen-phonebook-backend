bbd99c1061ce2ba112c60ea479450f159e6253e6
changsyuanwu <william.wu1219@gmail.com>
2024-12-04 08:46:09 +0800

    add delete functionality and move error handling to middleware

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const morgan = require("morgan");
 const cors = require("cors");
 const Person = require("./models/person")
 
-const app = express();
-app.use(express.json());
-app.use(express.static("dist"));
-app.use(cors());
-
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
 });
 
+const app = express();
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(cors());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
@@ -22,32 +21,11 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+let persons = [];
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (req, res) => {
   Person.find({}).then(persons => {
-    response.json(persons);
+    res.json(persons);
   })
 });
 
@@ -76,23 +54,23 @@ app.post("/api/persons", (req, res) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
+app.get("/api/persons/:id", (req, res, next) => {
+  const id = req.params.id;
   Person.findById(id)
     .then((p) => {
-      response.json(p);
+      res.json(p);
     })
-    .catch((err) => {
-      response.status(404).end();
-      console.log(err);
-    });
+    .catch((err) => next(err));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res, next) => {
   const id = req.params.id;
-  const person = persons.find((p) => p.id === id);
-  persons = persons.filter((p) => p.id !== id);
-  res.status(200).json(person);
+  Person.findByIdAndDelete(id)
+    .then((result) => {
+      if (result) res.status(200).json(result);
+      else res.status(204).end();
+    })
+    .catch((err) => next(err));
 });
 
 app.get("/api/info", (req, res) => {
@@ -101,3 +79,15 @@ app.get("/api/info", (req, res) => {
     `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
   );
 });
+
+const errorHandler = (err, req, res, next) => {
+  console.error(err.message);
+
+  if (err.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+
+  next(err);
+};
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler);
